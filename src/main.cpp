6f1b34264bb4452058d7b0e6b780d8c1a4d00db9
faisalfarzana <farzanaf35@gmail.com>
2026-09-13 15:32:24 -0700

    initial opencv and library dev environment

--- src/main.cpp
+++ src/main.cpp
@@ -20,3 +20,55 @@ L3: Draw output
 // 6. Draw tracking circle at (cx, cy) on display frame.
 // 7. Swap frame pointers: previous_frame = current_frame.
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "device.h"
+#include "kernel.h"
+#include "matrix.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#define CHECK_ERR(err, msg)                           \
+    if (err != CL_SUCCESS)                            \
+    {                                                 \
+        fprintf(stderr, "%s failed: %d\n", msg, err); \
+        exit(EXIT_FAILURE);                           \
+    }
+
+#define KERNEL_PATH "kernel.cl"
+
+
+int main() {
+
+    cv::VideoCapture cap("video/Video.mov"); // find video
+
+    if (!cap.isOpened()) {
+        std::cerr << "Error: could not open video\n";
+        return -1;
+    }
+
+    cv::Mat frame;
+
+    while (cap.read(frame)) {
+        std::cout << "Read frame successfully\n";
+
+    }
+
+    std::cout << "FPS: "
+          << cap.get(cv::CAP_PROP_FPS)
+          << "\n";
+
+    std::cout << "Frame count: "
+            << cap.get(cv::CAP_PROP_FRAME_COUNT)
+            << "\n";
+
+    std::cout << "Duration: "
+            << cap.get(cv::CAP_PROP_FRAME_COUNT) /
+                cap.get(cv::CAP_PROP_FPS)
+            << " seconds\n";
+
+    cap.release();
+    return 0;
+}
